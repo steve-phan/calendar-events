3675525d89460c403cca28ec8167e78a3ec8a73d
Steve Phan <phanhaingoc@gmail.com>
2022-10-06 00:11:56 +0200

    deploy to netlify

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -29,7 +29,7 @@ const Calendar = () => {
     async () => {
       const response = await axios.post(
         // TODO: replace online API
-        "http://localhost:8888/.netlify/functions/handle-events-calendar",
+        "/.netlify/functions/handle-events-calendar",
         JSON.stringify({
           eventType: "GET_EVENTS",
         })
